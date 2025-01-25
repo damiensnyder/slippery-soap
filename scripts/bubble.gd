f92ea8a265e39995893381abda4f6a7043996545
Damien Snyder <signup@damiensnyder.com>
2025-01-25 12:57:18 -0500

    change to tau

--- scripts/bubble.gd
+++ scripts/bubble.gd
@@ -53,17 +53,43 @@ func _physics_process(delta):
 		new_blullet.position = position - rightgun_direction*150
 		get_node("/root/GameManager").add_child(new_blullet)
 	
+	if Input.is_action_just_pressed("shoot left"):
+		angular_velocity += 0.005 * lateral_recoil
+		velocity += leftgun_direction * lateral_recoil
+		
+		left_gun_anim.play()
+		
+		var new_blullet = BLULLET.instantiate()
+		new_blullet.velocity = -leftgun_direction * bullet_speed
+		new_blullet.position = position - leftgun_direction*150
+		get_node("/root/GameManager").add_child(new_blullet)
+	
+	if Input.is_action_just_pressed("shoot forward"):
+		velocity += (leftgun_direction + rightgun_direction) * lateral_recoil
+		
+		left_gun_anim.play()
+		right_gun_anim.play()
+		
+		var left_blullet = BLULLET.instantiate()
+		left_blullet.velocity = -leftgun_direction * bullet_speed
+		left_blullet.position = position - leftgun_direction*150
+		get_node("/root/GameManager").add_child(left_blullet)
+		var right_blullet = BLULLET.instantiate()
+		right_blullet.velocity = -rightgun_direction * bullet_speed
+		right_blullet.position = position - rightgun_direction*150
+		get_node("/root/GameManager").add_child(right_blullet)
+	
 	angular_velocity = clampf(angular_velocity, -0.05, 0.05)
 	#velocity = Vector2(
 		#clamp(velocity.x, -5, 5),
 		#clamp(velocity.y, -5, 5)
 	#)
-	velocity += Vector2(0,gravity*0.005)
-	velocity = lerp(velocity, Vector2(0,0), 0.01 * velocity.length() * lateral_air_friction)
+	velocity += Vector2(0, gravity * 0.005)
+	velocity = lerp(velocity, Vector2(0, 0), 0.01 * velocity.length() * lateral_air_friction)
 	angular_velocity = lerp(angular_velocity, 0.0, 0.01 * angular_air_friction)
 	
 	rotation += angular_velocity
-	rotation = fmod(rotation + 2*PI, 2*PI)
+	rotation = fmod(rotation + TAU, TAU)
 	var collision = move_and_collide(velocity)
 	
 	if collision and collision.get_collider().has_method("is_wall"):
