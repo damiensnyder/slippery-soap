64cf2d95a6805dc9caf45c48223660a21be33bb3
henry <henryferg4@gmail.com>
2025-01-25 18:06:23 -0500

    Particles

--- scripts/blullet.gd
+++ scripts/blullet.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 
-enum states {INAIR, POSTHIT}
+@onready var bubble_explosion = preload("res://scenes/gpu_particles_2d.tscn")
+@onready var explosion_parent = $"/root/GameManager"
+enum states {INAIR, POSTHIT, KILLSELF}
 var state = states.INAIR
 
 # Called when the node enters the scene tree for the first time.
@@ -13,19 +15,21 @@ func _physics_process(delta):
 		states.INAIR:   inair_state()
 		states.POSTHIT: posthit_state() # this state change happens in the script for the node the blullet hits. sorry!
 
-
 func inair_state():
 	var collision = move_and_collide(velocity)
 	if collision: state = states.POSTHIT
-	
 
 func posthit_state():
-	move_and_collide(velocity)
-	scale = lerp(scale, Vector2(2,2), 0.2)
-	velocity = lerp(velocity, Vector2(0,0), 0.05)
-	modulate.a = lerp(modulate.a, 0.0, 0.2)
-	if modulate.a < 0.01: queue_free()
-
+	#move_and_collide(velocity)
+	#scale = lerp(scale, Vector2(2,2), 0.2)
+	#velocity = lerp(velocity, Vector2(0,0), 0.05)
+	#modulate.a = lerp(modulate.a, 0.0, 0.2)
+	#if modulate.a < 0.01:
+	var explosion = bubble_explosion.instantiate()
+	explosion_parent.add_child(explosion)
+	explosion.global_position = self.global_position
+	print(explosion)
+	self.queue_free()
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
-	queue_free()
+	self.queue_free()
---
+++ scripts/gpu_particles_2d.gd
@@ -0,0 +1,13 @@
+extends GPUParticles2D
+
+@onready var death_timer = $Timer
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	emitting = true
+	#print("particle explosion")
+	death_timer.start()
+
+func _on_timer_timeout():
+	print("destroy")
+	queue_free()
