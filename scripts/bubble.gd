9fb7f64dd65106d3a298710eb79a6e3e8ff5b6e6
Damien Snyder <signup@damiensnyder.com>
2025-01-25 13:29:11 -0500

    some epic physics changes

--- scripts/bubble.gd
+++ scripts/bubble.gd
@@ -6,10 +6,10 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var angular_velocity = 0
 
 var lateral_recoil = 5
-var angular_recoil = 5
+var angular_recoil = 10
 var bullet_speed = 10
 var lateral_air_friction = 1
-var angular_air_friction = 1
+var angular_air_friction = 2
 
 @onready var left_gun_anim = $LeftGun/LeftGunAnim
 @onready var right_gun_anim = $RightGun/RightGunAnim
@@ -26,8 +26,8 @@ func _physics_process(delta):
 	debug_controls()
 	
 	var direction = Vector2(cos(rotation), sin(rotation))
-	var leftgun_rotation = atan2(direction.y, direction.x) + PI/4
-	var rightgun_rotation = atan2(direction.y, direction.x) - PI/4
+	var leftgun_rotation = atan2(direction.y, direction.x) # + PI/4
+	var rightgun_rotation = atan2(direction.y, direction.x) # - PI/4
 	var leftgun_direction = Vector2(cos(leftgun_rotation), sin(leftgun_rotation))
 	var rightgun_direction = Vector2(cos(rightgun_rotation), sin(rightgun_rotation))
 	
@@ -39,7 +39,7 @@ func _physics_process(delta):
 		
 		var new_blullet = BLULLET.instantiate()
 		new_blullet.velocity = -leftgun_direction * bullet_speed
-		new_blullet.position = position - leftgun_direction*150
+		new_blullet.position = position + 80 * Vector2(direction.y, -direction.x) - 70 * direction
 		get_node("/root/GameManager").add_child(new_blullet)
 
 	if Input.is_action_just_pressed("shoot right"):
@@ -50,7 +50,7 @@ func _physics_process(delta):
 		
 		var new_blullet = BLULLET.instantiate()
 		new_blullet.velocity = -rightgun_direction * bullet_speed
-		new_blullet.position = position - rightgun_direction*150
+		new_blullet.position = position - 80 * Vector2(direction.y, -direction.x) - 70 * direction
 		get_node("/root/GameManager").add_child(new_blullet)
 	
 	if Input.is_action_just_pressed("shoot forward"):
@@ -61,19 +61,15 @@ func _physics_process(delta):
 		
 		var left_blullet = BLULLET.instantiate()
 		left_blullet.velocity = -leftgun_direction * bullet_speed
-		left_blullet.position = position - leftgun_direction*150
+		left_blullet.position = position + 80 * Vector2(direction.y, -direction.x) - 70 * direction
 		get_node("/root/GameManager").add_child(left_blullet)
 		var right_blullet = BLULLET.instantiate()
 		right_blullet.velocity = -rightgun_direction * bullet_speed
-		right_blullet.position = position - rightgun_direction*150
+		right_blullet.position = position - 80 * Vector2(direction.y, -direction.x) - 70 * direction
 		get_node("/root/GameManager").add_child(right_blullet)
 	
 	angular_velocity = clampf(angular_velocity, -0.05, 0.05)
-	#velocity = Vector2(
-		#clamp(velocity.x, -5, 5),
-		#clamp(velocity.y, -5, 5)
-	#)
-	velocity += Vector2(0, gravity * 0.005)
+	velocity += Vector2(0, gravity * 0.004)
 	velocity = lerp(velocity, Vector2(0, 0), 0.01 * velocity.length() * lateral_air_friction)
 	angular_velocity = lerp(angular_velocity, 0.0, 0.01 * angular_air_friction)
 	
@@ -88,23 +84,23 @@ func _physics_process(delta):
 	
 
 func debug_controls():
-	if Input.is_action_just_pressed("gravity up"): gravity += 0.1
-	if Input.is_action_just_pressed("gravity down"): gravity -= 0.1
+	if Input.is_action_just_pressed("gravity up"): gravity += 0.7
+	if Input.is_action_just_pressed("gravity down"): gravity -= 0.7
 
-	if Input.is_action_just_pressed("lateral recoil up"): lateral_recoil += 0.1
-	if Input.is_action_just_pressed("lateral recoil down"): lateral_recoil -= 0.1
+	if Input.is_action_just_pressed("lateral recoil up"): lateral_recoil += 0.5
+	if Input.is_action_just_pressed("lateral recoil down"): lateral_recoil -= 0.5
 	
-	if Input.is_action_just_pressed("angular recoil up"): angular_recoil += 0.1
-	if Input.is_action_just_pressed("angular recoil down"): angular_recoil -= 0.1
+	if Input.is_action_just_pressed("angular recoil up"): angular_recoil += 0.5
+	if Input.is_action_just_pressed("angular recoil down"): angular_recoil -= 0.5
 
-	if Input.is_action_just_pressed("bullet speed up"): bullet_speed += 0.1
-	if Input.is_action_just_pressed("bullet speed down"): bullet_speed -= 0.1
+	if Input.is_action_just_pressed("bullet speed up"): bullet_speed += 1
+	if Input.is_action_just_pressed("bullet speed down"): bullet_speed -= 1
 
-	if Input.is_action_just_pressed("lateral friction up"): lateral_air_friction += 0.1
-	if Input.is_action_just_pressed("lateral friction down"): lateral_air_friction -= 0.1
+	if Input.is_action_just_pressed("lateral friction up"): lateral_air_friction += 0.2
+	if Input.is_action_just_pressed("lateral friction down"): lateral_air_friction -= 0.2
 
-	if Input.is_action_just_pressed("angular friction up"): angular_air_friction += 0.1
-	if Input.is_action_just_pressed("angular friction down"): angular_air_friction -= 0.1
+	if Input.is_action_just_pressed("angular friction up"): angular_air_friction += 0.2
+	if Input.is_action_just_pressed("angular friction down"): angular_air_friction -= 0.2
 
 
 func is_bubble():
