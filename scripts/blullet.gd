d8cdf082071dbfd18fe2985a1701826aa541638c
YuvalGitHub <yuvaltalby@gmail.com>
2025-01-25 16:46:43 -0500

    added asset and working on auto aim

--- scripts/blullet.gd
+++ scripts/blullet.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+enum states {INAIR, POSTHIT}
+var state = states.INAIR
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -7,8 +9,18 @@ func _ready():
 
 
 func _physics_process(delta):
+	match state:
+		states.INAIR:   inair_state()
+		states.POSTHIT: posthit_state()
+
+
+func inair_state():
 	var collision = move_and_collide(velocity)
 	if collision: queue_free()
+	
+
+func posthit_state():
+	move_and_collide(velocity)
 
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
