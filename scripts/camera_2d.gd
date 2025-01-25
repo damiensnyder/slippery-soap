30223894a1f7aad889deb31f68c5b85f4f03a96d
henry <henryferg4@gmail.com>
2025-01-25 15:27:17 -0500

    add screen shake

---
+++ scripts/camera_2d.gd
@@ -0,0 +1,30 @@
+extends Camera2D
+
+@export var random_shake_strength: float = 40.0
+@export var decay: float = 0.6 #how quickly shaking will stop
+@onready var rand = RandomNumberGenerator.new()
+@onready var shake = 0.0 #the shake being applied
+
+func _ready() -> void:
+	rand.randomize()
+	Globals.connect("shake_screen", apply_shake)
+
+func apply_shake():
+	shake = random_shake_strength
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	#always be making shake strength lerp to zero
+	shake = lerp(shake, 0.0, decay)
+	
+	offset = _get_random_offset()
+	
+	if Input.is_action_pressed("ui_left"):
+		apply_shake()
+
+func _get_random_offset():
+	var rand_move = Vector2(
+		rand.randf_range(-shake, shake),
+		rand.randf_range(-shake, shake)
+	)
+	return rand_move
--- scripts/globals.gd
+++ scripts/globals.gd
@@ -1,8 +1,13 @@
 extends Node2D
 
+signal shake_screen()
+
 var player_position = Vector2(0,0)
 
 var recoil = 5
 var bullet_speed = 10
 var lateral_air_friction = 1
 var angular_air_friction = 1
+
+func emit_screenshake():
+	emit_signal("shake_screen")
