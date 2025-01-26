93e5dd5c5c871a6fb94509260dced4e0a0ca8822
henry <henryferg4@gmail.com>
2025-01-25 19:43:58 -0500

    New soap

--- scripts/soap.gd
+++ scripts/soap.gd
@@ -2,47 +2,45 @@ extends CharacterBody2D
 
 enum states {BIRTH, LIFE, DEATH}
 var state = states.BIRTH
+@onready var lerp_speed = 0.05
+@onready var player = $"/root/GameManager/Bubble"
+@onready var rng = RandomNumberGenerator.new()
+@onready var timer = $Timer
+@onready var rand_speed_max_min = 7
 
 var target_scale = Vector2(0.5,0.5)
 var lifespan : float = 5.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	scale = target_scale
+	velocity += Vector2(rng.randf_range(-rand_speed_max_min, rand_speed_max_min), rng.randf_range(-rand_speed_max_min, rand_speed_max_min))
 
 func _physics_process(delta):
 	match state:
 		states.BIRTH: birth_state()
 		states.LIFE: life_state()
 		states.DEATH: death_state()
-		
-	velocity = lerp(velocity, Vector2(0,0), 0.05)
-	var collision = move_and_collide(velocity)
-	if collision: velocity = velocity.bounce(collision.get_normal())
-
-
+		#
 func birth_state():
-	scale = lerp(scale, target_scale, 0.05)
-	modulate.a = scale.x / target_scale.x # so that the relative opacity is the same as relative size
-	if scale.x > target_scale.x - 0.01: #if it's close enough, that is
-		scale = target_scale
-		state = states.LIFE
-	
-	
-func life_state():
-	modulate.a -= 1.0 / 100 / lifespan
-	if modulate.a <= 0: queue_free()
+	move_and_collide(velocity)
+	velocity = lerp(velocity, Vector2(0,0), 0.05)
+	if velocity == Vector2(0,0):
+		state = states.LIFE #this and timer trigger state change
+	#pass
 
+func life_state():
+	global_position = lerp(global_position, player.global_position, lerp_speed)
 
 func death_state():
-	scale = lerp(scale, target_scale*2, 0.05)
-	if scale.x > target_scale.x * 2 - 0.01: queue_free()
-	modulate.a -= 1.0 / 25
-
+	queue_free()
 
 func _on_area_2d_body_entered(body):
 	if state == states.DEATH: return
 	
 	if body.has_method("is_bubble"):
 		state = states.DEATH
+
+
+func _on_timer_timeout() -> void:
+	state = states.LIFE
