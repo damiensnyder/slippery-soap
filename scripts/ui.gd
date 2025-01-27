d8b397ad93db4b17713d5b1ad1d6a77234bac694
henry <henryferg4@gmail.com>
2025-01-27 14:04:15 -0500

    progress bar

--- scripts/ui.gd
+++ scripts/ui.gd
@@ -1,12 +1,17 @@
 extends CanvasLayer
 
 @onready var AMMOSPRITE = preload("res://assets/bullet1.png")
+@onready var PROGRESSMARKER = preload("res://assets/bubble_placeholder.png")
 @onready var ammo_origin = $AmmoPlace
 @onready var soap_text = $SoapCounter
 #@onready var current_ammo = Globals.ammo
 @onready var ammo_size = 40 #hardcoding sowwy
 @onready var max_ammo_in_line = (1920/40) - 2
 @onready var ammo_array = []
+@onready var player_sprite = null
+@onready var progress_line = $Line2D
+@onready var player_marker_start = $PlayerMarkerStart
+@onready var win_position = $"../FinishLine"
 var ammo_check = Globals.ammo
 
 func _ready() -> void:
@@ -26,6 +31,7 @@ func _ready() -> void:
 		p += 1
 
 func _physics_process(_delta: float) -> void:
+	var player = get_node_or_null("../Bubble")
 	soap_text.text = "= " + str(Globals.soap)
 	if Globals.ammo <= 0:
 		for n in (ammo_check - Globals.ammo):
@@ -71,5 +77,21 @@ func _physics_process(_delta: float) -> void:
 				add_child(new_ammo_sprite)
 				
 				p += 1
-	
 		ammo_check = Globals.ammo
+
+	if player != null and player_sprite == null: #progress bar
+		player_sprite = Sprite2D.new()
+		player_sprite.texture = PROGRESSMARKER
+		player_sprite.scale = Vector2(0.2, 0.2)
+		player_sprite.global_position = player_marker_start.global_position
+		add_child(player_sprite)
+		#player.global_position
+		print(player_sprite)
+	if player_sprite != null:
+		var total_dist = abs(win_position.global_position.y) + 760 #760 is start, winposition is negative
+		var current_position_percent = (player.global_position.y - win_position.global_position.y)/total_dist
+		var line_total_dist = progress_line.global_position.y - player_marker_start.global_position.y
+		current_position_percent = (1 - current_position_percent)
+		player_sprite.global_position.y = player_marker_start.global_position.y + (line_total_dist * current_position_percent)
+		print(player_sprite.global_position.y)
+		player_sprite.global_position.y = clamp(player_sprite.global_position.y, progress_line.global_position.y, player_marker_start.global_position.y)
